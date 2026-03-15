7bd19cce9e6262beb9d30e784ecb8e4c93a3fcca
Avanshu Diwane <avanshudiwane0@gmail.com>
2026-03-15 22:14:13 +0530

    fix: first login wait  issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react"; // Import useEffect
+import axios from "axios"; // Import axios
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
 import AddExpense from "./Pages/AddExpense";
 
 function App() {
+  
+ useEffect(() => {
+    const wakeUp = async () => {
+      try {
+        // This pings your specific Render link
+        await axios.get("https://smart-expense-4g4x.onrender.com/api/users"); 
+      } catch (err) {
+        // We don't care about the error, we just want the server to wake up!
+        console.log("Please wait...");
+      }
+    };
+    wakeUp();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
-
         <Route path="/add-expense" element={<AddExpense />} />
-
       </Routes>
     </BrowserRouter>
   );
